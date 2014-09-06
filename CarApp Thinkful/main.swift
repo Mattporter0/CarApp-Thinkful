859f420e53dcbbe864978dbe5507ca88e6713354
Mattporter0 <mattporter@Matts-MacBook-Pro.local>
2014-09-06 16:40:47 +0800

    Initial Commit

---
+++ CarApp Thinkful/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CarApp Thinkful
+//
+//  Created by Matt Porter on 06/09/2014.
+//  Copyright (c) 2014 Matt Porter. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
